b2536373cbb19326281dd99230caaa5d2892c2a5
Joy-Wanjiru <wanjirujoymuhia@gmail.com>
2018-09-23 15:02:32 +0300

    Add initial code to html and js files

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $("form#quizboard").submit(function(event){
+    var question1= $("input:radio[name=question1]:checked").val();
+    var question2= $("input:radio[name=question2]:checked").val();
+    var question3= $("input:radio[name=question3]:checked").val();
+    var question4= $("input:radio[name=question4]:checked").val();
+    var question5= $("input:radio[name=question5]:checked").val();
+
+    var result=parseInt(question1)+parseInt(question2)+parseInt(question3)+parseInt(question4)+parseInt(question5);
+    $("#result").text("Great job! Your total score is: " +result + "%.");
+
+    $("button").click(function() {
+      $("#result-showing").toggle();
+      $("#quizboard-hidden").toggle();
+    });
+      $("form#quizboard").hide();
+      $("#result").show();
+
+      event.preventDefault();
+    });
+});
